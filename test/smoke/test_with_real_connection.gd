7b1f99a712e8e84ba04d3297656d88a085f4d8a2
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2025-12-25 01:06:49 +0100

    wrote a kinda useless test

--- test/smoke/test_with_real_connection.gd
+++ test/smoke/test_with_real_connection.gd
@@ -1,4 +1,23 @@
 extends GutTest
+const PluginStub := preload("res://test/stubs/plugin_stub.gd")
+const DontChangeThisScript:= preload("res://test/stubs/dont_change_this_script.gd")
+var plugin := PluginStub.new(true);
+func before_all():
+	add_child(plugin);
+func after_all():
+	remove_child(plugin)
+	plugin.free();
+func after_each():
+	plugin.code_edit_handler.remove_code_edit();
+	plugin.nvim_buffer_manager.detach_buffers();
+static func get_some_script_path()->String:
+	return ProjectSettings.globalize_path((DontChangeThisScript as Script).resource_path);
 
 func test_shift_j():
-	pass
+	plugin.code_edit_handler.caret_moved.connect(func():assert_true(false));
+	plugin.editor_events.file_changed.emit(get_some_script_path())
+	plugin.code_edit_handler.code_edit.text = (DontChangeThisScript as Script).source_code
+	for i in range(20):
+		plugin.nvim_api_requester.send_input("<S-j>")
+		plugin.nvim_api_requester.send_input("u")
+	assert_true(true);
--- test/stubs/plugin_stub.gd
+++ test/stubs/plugin_stub.gd
@@ -4,16 +4,18 @@ const CodeEditHandlerStub:= preload("res://test/stubs/code_edit_handler_stub.gd"
 const NvimConnectionStub:= preload("res://test/stubs/nvim_connection_stub.gd")
 const EditorSetupStub:=preload("res://test/stubs/editor_setup_stub.gd")
 const NvimBufferManagerStub:= preload("res://test/stubs/nvim_buffer_manager_stub.gd")
+var real_connection_flag:= false
 
-func _notification(what: int) -> void:
-	if what != NOTIFICATION_ENTER_TREE:
-		return
+func _init(real_connection:= false):
+	real_connection_flag = real_connection
+func _enter_tree():
 	code_edit_handler = CodeEditHandlerStub.constructor(self);
 	editor_setup = EditorSetupStub.new();
-	nvim_connection = NvimConnectionStub.new();
+	if not real_connection_flag: 
+		nvim_connection = NvimConnectionStub.new();
 	nvim_buffer_manager = NvimBufferManagerStub.new();
 	super._enter_tree();
-func _enter_tree():
 	editor_events.file_changed.disconnect(code_edit_handler.set_code_edit)
 func _exit_tree():
+	super._exit_tree()
 	code_edit_handler.cleanup();
